0317ecad10e6fe5e6bd428d0160b4561dcab8d7e
Shailes Pethkar <104692252+Sp-007-tech@users.noreply.github.com>
2023-03-30 13:45:13 +0530

    Updated !

--- form.php
+++ form.php
@@ -2,11 +2,10 @@
 
 <html lang="en">
 <head>
-    
+<!--Title -->
     <title> Form Sample ! </title>
     
-</head>
-    
+</head>   
 <body>
     
     <?php
@@ -33,14 +32,19 @@
     <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
 
     Name: <input type="text" name="name"><br><br>
+        
     E-mail: <input type="text" name="email"><br><br>
+        
     Website: <input type="text" name="website"><br><br>
+        
     Comment: <textarea name="comment" rows="5" cols="20"></textarea><br><br>
+        
     Gender: 
     <input type="radio" name="gender" value="female">Female
     <input type="radio" name="gender" value="male">Male
-    <input type="radio" name="gender" value="other">other
+    <input type="radio" name="gender" value="other">Other
     <br><br>
+        
     <input type="Submit" name="submit" value="Submit">
         
     <?php 
